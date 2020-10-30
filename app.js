638fc9a7dde889cbb2a30450b5e3f1e521fa02e2
Julien Dellys <julien.dellys.pro@gmail.com>
2020-10-30 11:56:31 +0100

    Final pas fini

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+//const Data = require("./back/Data/Models/card_models");
 // var materialize = require("materialize-css");
 const port = 3000;
+const axios = require("axios");
+let data = null;
 
 const cardRoute = require("./back/Routes/card_routes");
 app.use(bodyParser.json());
@@ -14,6 +17,15 @@ app.set("view engine", "ejs");
 
 app.use("/card", cardRoute);
 
+axios.get('http://localhost:3000/card/get')
+.then(function (response) {
+    onSuccess(response)
+    //console.log(response);
+    //data = reponse;
+})
+.catch(function (error) {
+    console.log(error);
+});
 
 // Chargement de la page index.html
 app.get('/', (req, res) => {
@@ -26,6 +38,8 @@ app.get('/catalogue', (req, res) => {
 
 app.get('/detail/:id', (req, res) => {
     res.render('detail', { id: req.params.id});
+    console.log(data);
+
 });
 
 app.get('/connect', (req, res) => {
@@ -40,3 +54,40 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
+function onSuccess(response) {
+    const array = response;
+    const arrayLength = Object.keys(array).length 
+    console.log(arrayLength)
+    for(let i = 0; i <= arrayLength; i++) {
+        //*
+        const title = array.data[i].title;
+        const cours = array.data[i].cours;
+        const description = array.data[i].description;
+        const image = array.data[i].image;
+        const time = array.data[i].time;
+        const ranking = array.data[i].ranking;
+        //*/
+        //console.log(`${cours}, ${title}, ${description}, ${image}, ${time}, ${ranking}`);
+        //assignDataValue(cours, title, description, image, time, ranking)
+    }
+    data[0] = title;
+    data[1] = cours;
+    data[2] = description;
+    data[3] = image;
+    data[4] = time;
+    data[5] = ranking;
+}
+    
+    function assignDataValue(cours, title, description, image, time, ranking) {
+    
+        const upData = new Data()
+        upData.cours = cours;
+        upData.title = title;
+        upData.description = description;
+        upData.image = image;
+        upData.time = time;
+        upData.ranking = ranking;
+    
+        //upData.save();
+        return upData;
+    }
--- back/Data/data.js
+++ back/Data/data.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const mongoose = require("mongoose");
 const Data = require("../Data/Models/card_models");
 
@@ -12,49 +11,6 @@ mongoose.connection.once("open", () => {
 .on("error", (error) => {
     console.log("on n'est pas connecté" + error)
 });
-
-axios.get('http://localhost:3000/card/get')
-.then(function (response) {
-    onSuccess(response)
-    console.log(response);
-})
-.catch(function (error) {
-    console.log(error);
-});
-
-
-function onSuccess(response) {
-    const array = response;
-    const arrayLength = Object.keys(array).length 
-    console.log(arrayLength)
-    for(let i = 0; i <= arrayLength; i++) {
-        const cours = array.data[i].cours;
-        const title = array.data[i].title;
-        const description = array.data[i].description;
-        const image = array.data[i].image;
-        const time = array.data[i].time;
-        const ranking = array.data[i].ranking;
-        console.log(`${cours}, ${title}, ${description}, ${image}, ${time}, ${ranking}`);
-    
-        assignDataValue(cours, title, description, image, time, ranking)
-    
-        }
-    }
-    
-    function assignDataValue(cours, title, description, image, time, ranking) {
-    
-        const upData = new Data()
-        upData.cours = cours;
-        upData.title = title;
-        upData.description = description;
-        upData.image = image;
-        upData.time = time;
-        upData.ranking = ranking;
-    
-        upData.save();
-    }
-
-
 
 
 
